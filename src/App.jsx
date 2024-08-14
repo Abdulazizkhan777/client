2e9ef5d557b75e70434fadd6a8480e4dc4007220
Abdulazizkhan777 <abdulazizkhan107925@gmail.com>
2024-08-14 17:38:34 +0500

    third change

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Signup from './Signup'
 import Login from './Login'
 import Home from './Home'
 import Users from './Users'
 import Createuser from './CreateUser'
 import UpdateUser from './UpdateUser'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <BrowserRouter>
-   <Routes>
-   <Route path='/register' element={<Signup/>}></Route>
-   <Route path='/login' element={<Login/>}></Route>
-   <Route path='/home' element={<Home/>}></Route>
-   <Route path='/users' element={<Users/>}></Route>
-   <Route path='/create' element={<Createuser/>}></Route>
-   <Route path='/update/:id' element={<UpdateUser/>}></Route>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/create' element={<Createuser />} />
+        <Route path='/update/:id' element={<UpdateUser />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
