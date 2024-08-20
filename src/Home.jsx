aa53adcb1a9bd0877855dce97aa8d9a5c08ff699
Abdulazizkhan777 <105599579+Abdulazizkhan777@users.noreply.github.com>
2024-08-20 13:38:30 +0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ function Home()
   axios.defaults.withCredentials = true;
   useEffect(()=>
   {
-    axios.get("https://crud-server-ruby-alpha.vercel.app//home")
+    axios.get("https://crud-server-ruby-alpha.vercel.app/home")
     .then(result=>{console.log(result)
       if(result !=="Success")
       {
